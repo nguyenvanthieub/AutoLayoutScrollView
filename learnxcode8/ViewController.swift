79d3609a92c24b5f35c327b31ee3c5a86017c382
Thieu Mao <nguyen.van.thieub@framgia.com>
2016-11-05 20:31:04 +0700

    AutoLayout ScrollView Example

---
+++ learnxcode8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  learnxcode8
+//
+//  Created by Thieu Mao on 11/5/16.
+//  Copyright © 2016 thieumao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
